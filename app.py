85c0d8f15a5a2e9493c18db21ba69f785d7da126
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2021-01-09 19:35:28 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import  Flask, render_template, url_for, redirect, request, send_file
+from flask_sqlalchemy import SQLAlchemy
+''' create instance of Flask web app '''
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+db.create_all()
+
+''' The decoration is to give a route to Flask to access this method '''
+''' in the route parameter is to give a path to the method (web query) '''
+@app.route("/")
+def home():
+    allItems = Item.query.all()
+    return render_template('home.html', allItems=allItems)
+
+@app.route('/add', methods=['POST'])
+def add():
+    item = Item(text=request.form['items'], complete=False)
+    db.session.add(item)
+    db.session.commit()
+
+    return redirect(url_for('home'))
+
+@app.route('/complete', methods=['POST'])
+def complete():
+    id = request.form.get('hello')
+    item = Item.query.filter_by(id=int(id)).first()
+
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
